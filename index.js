33a63fb23f6a52871418356dea940beb8840c05c
Mani Movassagh <mani.mgh@gmail.com>
2022-07-13 00:28:12 +0200

    use lodash

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const _ = require('lodash');
 
-const obj=[{ss:'a'},{sam:'b'},{sss:'c'}];
+const obj = [{ ss: 'a' }, { sam: 'b' }, { sss: 'c' }];
 
-console.log(obj.reduce(function(result, current) {
+console.log(obj.reduce(function (result, current) {
   return Object.assign(result, current);
 }, {}))
 
-const arrObj = [{a: 1, b: 2}, {c: 3, d: 4}, {e: 5, f: 6}]
-const m=arrObj.reduce(((r, c) => Object.assign(r, c)), {})
+const arrObj = [{ a: 1, b: 2 }, { c: 3, d: 4 }, { e: 5, f: 6 }]
+const m = arrObj.reduce(((r, c) => Object.assign(r, c)), {})
 console.log(m);
+
+const n = arrObj.reduce(((r, c) => Object.assign(r, c)), {})
+console.log(n);
