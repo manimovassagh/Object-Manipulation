c7a7340205c2564343cb5afbdbde04a653ef5dad
Mani Movassagh <mani.mgh@gmail.com>
2022-07-13 00:23:09 +0200

    Object Manipulation

---
+++ index.js
@@ -0,0 +1,14 @@
+const _ = require('lodash');
+
+
+const obj=[{ss:'a'},{sam:'b'},{sss:'c'}];
+
+
+
+
+console.log(obj.reduce(function(result, current) {
+  return Object.assign(result, current);
+}, {}))
+
+const arrObj = [{a: 1, b: 2}, {c: 3, d: 4}, {e: 5, f: 6}]
+console.log(arrObj.reduce(((r, c) => Object.assign(r, c)), {}));
